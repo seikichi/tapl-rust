e4de7659800cb8eded4c4a4c6157d5499556fd05
Seikichi KONDO <seikichi@kmc.gr.jp>
2020-08-03 03:22:49 +0900

    Add parser in untyped

--- untyped/src/main.rs
+++ untyped/src/main.rs
@@ -1,5 +1,15 @@
 use std::rc::Rc;
 
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{char, digit1, multispace0},
+    combinator::{map, map_opt},
+    multi::fold_many1,
+    sequence::{delimited, pair},
+    IResult,
+};
+
 #[derive(Debug, PartialEq)]
 enum Term {
     Var(i32),
@@ -65,20 +75,89 @@ fn eval(t: Rc<Term>) -> Rc<Term> {
     return t;
 }
 
-fn main() {
-    // (λ. 1 0 2) (λ. 0) -> 0 (λ. 0) 1
-    let term: Rc<Term> = Term::App(
-        Term::Abs(
+fn parse_atomic_term(input: &str) -> IResult<&str, Rc<Term>> {
+    delimited(
+        multispace0,
+        alt((
+            delimited(char('('), parse_term, char(')')),
+            map_opt(digit1, |d: &str| Some(Rc::new(Term::Var(d.parse().ok()?)))),
+        )),
+        multispace0,
+    )(input)
+}
+
+fn parse_app_term(input: &str) -> IResult<&str, Rc<Term>> {
+    fold_many1(
+        parse_atomic_term,
+        Term::Var(-1).into(),
+        |t1: Rc<Term>, t2| {
+            if let Term::Var(-1) = &*t1 {
+                return t2;
+            }
+            Term::App(t1, t2).into()
+        },
+    )(input)
+}
+
+fn parse_lambda(input: &str) -> IResult<&str, Rc<Term>> {
+    map(
+        pair(alt((tag("lambda."), tag("λ."))), parse_term),
+        |(_, t)| Term::Abs(t).into(),
+    )(input)
+}
+
+fn parse_term(input: &str) -> IResult<&str, Rc<Term>> {
+    delimited(
+        multispace0,
+        alt((parse_app_term, parse_lambda)),
+        multispace0,
+    )(input)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (_, term) = parse_term("(λ. 1 0 2) (λ. 0)")?;
+    let result = eval(term.clone());
+    println!("{:?} -> {:?}", term, result); // expect `0 (λ. 0) 1`
+
+    Ok(())
+}
+
+#[test]
+fn test_parse() {
+    let tests: Vec<(&str, Rc<Term>)> = vec![
+        ("0", Term::Var(0).into()),
+        ("λ. 0", Term::Abs(Term::Var(0).into()).into()),
+        (
+            "0 1",
+            Term::App(Term::Var(0).into(), Term::Var(1).into()).into(),
+        ),
+        (
+            "0 1 2",
             Term::App(
-                Term::App(Term::Var(1).into(), Term::Var(0).into()).into(),
+                Term::App(Term::Var(0).into(), Term::Var(1).into()).into(),
                 Term::Var(2).into(),
             )
             .into(),
-        )
-        .into(),
-        Term::Abs(Term::Var(0).into()).into(),
-    )
-    .into();
-    let result = eval(term.clone());
-    println!("{:?} -> {:?}", term, result);
+        ),
+        (
+            "(λ. 1 0 2) (λ. 0)",
+            Term::App(
+                Term::Abs(
+                    Term::App(
+                        Term::App(Term::Var(1).into(), Term::Var(0).into()).into(),
+                        Term::Var(2).into(),
+                    )
+                    .into(),
+                )
+                .into(),
+                Term::Abs(Term::Var(0).into()).into(),
+            )
+            .into(),
+        ),
+    ];
+
+    for (input, expected) in tests {
+        let r = parse_term(input);
+        assert_eq!(r, Ok(("", expected)), "testing parse({})", input);
+    }
 }
